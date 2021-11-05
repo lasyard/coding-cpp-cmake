84128f2baad478d1b878d4a21a7065cf487b0deb
Yueguang Jiao <lasyard@yeah.net>
2021-11-05 18:27:30 +0800

    Added quiz/nine_interlocking_rings.

---
+++ quiz/nine_interlocking_rings.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+#define RINGS 9
+
+static void print(int pos[])
+{
+    for (int i = 0; i < RINGS; i++) {
+        printf("%2d", pos[i]);
+    }
+    printf("\n");
+}
+
+static void moveRing(int pos[], int ring, int to)
+{
+    static int steps = 0;
+    steps++;
+    pos[ring] = to;
+    printf("Step %3d: Ring %2d %-5s->", steps, ring, to == 1 ? "Up" : "Down");
+    print(pos);
+}
+
+static void moveAllRings(int pos[], int ring, int to)
+{
+    if (to == 1 && ring >= 1) {
+        moveAllRings(pos, ring - 1, 1);
+    }
+    if (ring >= 2) {
+        moveAllRings(pos, ring - 2, 0);
+    }
+    moveRing(pos, ring, to);
+    if (ring >= 2) {
+        moveAllRings(pos, ring - 2, 1);
+    }
+    if (to == 0 && ring >= 1) {
+        moveAllRings(pos, ring - 1, 0);
+    }
+}
+
+int main(void)
+{
+    int pos[RINGS];
+    for (int i = 0; i < RINGS; i++) {
+        pos[i] = 1;
+    }
+    moveAllRings(pos, RINGS - 1, 0);
+    return 0;
+}
